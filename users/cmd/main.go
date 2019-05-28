81d92e68946fa060783a1ec8bd31da4d751c49bb
Mike Flores <mflores@us.ci.org>
2019-05-28 10:26:05 -0700

    handle http post and get for get all users and insert new user

--- users/cmd/main.go
+++ users/cmd/main.go
@@ -1,20 +1,39 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/gorilla/mux"
+
+	"github.com/flores95/golang-curriculum-c-5/users/repo"
+	"github.com/flores95/golang-curriculum-c-5/users/user"
 )
+
+var store = repo.Repo{}
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/users", AllUsers).Methods("GET")
+	r.HandleFunc("/users", allUsers).Methods("GET")
+	r.HandleFunc("/users", createUser).Methods("POST")
 	http.ListenAndServe(":4180", r)
 }
 
-func AllUsers(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
+func allUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	j, _ := json.Marshal(store.GetAll())
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Category: %v\n", vars["category"])
+	w.Write(j)
+}
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var u user.User
+	b, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(b, &u)
+	store.Insert(u)
+	j, _ := json.Marshal(u)
+	w.WriteHeader(http.StatusOK)
+	w.Write(j)
 }
