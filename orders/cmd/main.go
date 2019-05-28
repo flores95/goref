c8e4c315b6887543c41a49d19f605e5dda7c9135
Mike Flores <mflores@us.ci.org>
2019-05-28 11:48:47 -0700

    add an endpoint for finding orders by user email

--- orders/cmd/main.go
+++ orders/cmd/main.go
@@ -13,27 +13,45 @@ import (
 
 var store = repo.Repo{}
 
+type OrderSearch struct {
+	UserEmail string
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/orders", allOrders).Methods("GET")
+	r.HandleFunc("/orders/find", findOrders).Methods("POST")
 	r.HandleFunc("/orders", createOrder).Methods("POST")
 	http.ListenAndServe(":4181", r)
 }
 
 func allOrders(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
 	j, _ := json.Marshal(store.GetAll())
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(j)
+}
+
+func findOrders(w http.ResponseWriter, r *http.Request) {
+	var os OrderSearch
+	b, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(b, &os)
+	j, _ := json.Marshal(store.GetOrders(os.UserEmail))
+
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(j)
 }
 
 func createOrder(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
 	var u order.Order
 	b, _ := ioutil.ReadAll(r.Body)
 	json.Unmarshal(b, &u)
 	store.Insert(u)
 	j, _ := json.Marshal(u)
+
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(j)
 }
