c3a00616b5af12c1a2ae70d13e3221f7efd63252
Mike Flores <mflores@us.ci.org>
2019-06-05 13:09:56 -0700

    fixed issue with MockLogger

--- cli/frameworks/logging/mocks.go
+++ cli/frameworks/logging/mocks.go
@@ -1,11 +1,18 @@
 package logging
 
 type MockLogger struct {
-	LogInvoked bool
-	LogFunc    func(LogEvent)
+	LogInvoked      bool
+	LogFunc         func(LogEvent)
+	SetLevelInvoked bool
+	SetLevelFunc    func(LogLevel)
 }
 
 func (m *MockLogger) Log(e LogEvent) {
 	m.LogInvoked = true
 	m.LogFunc(e)
+}
+
+func (m *MockLogger) SetLevel(l LogLevel) {
+	m.SetLevelInvoked = true
+	m.SetLevelFunc(l)
 }
